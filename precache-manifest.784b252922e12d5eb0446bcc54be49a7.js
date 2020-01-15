b718010dbff802c6ba229227476cc11d0a222fdb
Stuart Lynn <stuart.lynn@gmail.com>
2020-01-14 22:19:25 -0500

    Updates

---
+++ precache-manifest.784b252922e12d5eb0446bcc54be49a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "920d3c1c1eb9335f1f7dcf39bdd5988a",
+    "url": "/OpenToxMap/index.html"
+  },
+  {
+    "revision": "489c8b1f36e26f1f3b14",
+    "url": "/OpenToxMap/static/css/main.7a203b56.chunk.css"
+  },
+  {
+    "revision": "767dcd7df4e6d61958ec",
+    "url": "/OpenToxMap/static/js/2.f12323ec.chunk.js"
+  },
+  {
+    "revision": "ce3126c332c3bc8f1544c88c174f7c51",
+    "url": "/OpenToxMap/static/js/2.f12323ec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "489c8b1f36e26f1f3b14",
+    "url": "/OpenToxMap/static/js/main.7cef46b6.chunk.js"
+  },
+  {
+    "revision": "e1153801c8b77c0b995f",
+    "url": "/OpenToxMap/static/js/runtime-main.1faf2d58.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/OpenToxMap/static/media/logo.5d5d9eef.svg"
+  }
+]);
